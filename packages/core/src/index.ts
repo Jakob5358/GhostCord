d0622e91d9eda1c4d47d93142d13f33fc612f378
ThatGuyJamal <43446907+ThatGuyJamal@users.noreply.github.com>
2022-04-29 14:10:08 -0400

    Ghost Container

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,6 +2,7 @@ export * from "./structures/GhostClient";
 export * from "./structures/GhostCommand";
 export * from "./structures/GhostEvent";
 export * from "./structures/GhostPlugin";
+export * from "./structures/GhostConainter"
 
 export * from "./types/GhostOptions";
 export * from "./types/GhostEvent";
