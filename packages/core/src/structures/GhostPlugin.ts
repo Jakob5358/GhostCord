481580fa028dc8d041e916a0801660098a7dfc48
ThatGuyJamal <43446907+ThatGuyJamal@users.noreply.github.com>
2022-04-29 12:12:00 -0400

    Update GhostPlugin.ts

--- packages/core/src/structures/GhostPlugin.ts
+++ packages/core/src/structures/GhostPlugin.ts
@@ -4,6 +4,16 @@ export class GhostPluginManager {
     public activePlugins: Set<string> = new Set();
     public plugins: Map<string, GhostPluginType> = new Map()
 
+    public constructor() {
+        this.loadPlugins();
+    }
+
+    private loadPlugins() {
+        for (const plugin of this.plugins.values()) {
+            this.activePlugins.add(plugin.name);
+        }
+    }
+
     public get getActivePlugins() {
         return this.activePlugins;
     }
