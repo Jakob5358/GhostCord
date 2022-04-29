ed9872b66849803071648b4cac29ff8fb26d8b15
ThatGuyJamal <43446907+ThatGuyJamal@users.noreply.github.com>
2022-04-29 11:56:39 -0400

    Ghost Events

---
+++ packages/core/src/structures/GhostEvent.ts
@@ -0,0 +1,9 @@
+import { BaseEventsType, GhostEvent } from "../types/GhostEvent";
+
+/**
+ * Our event handler
+ * @param event The data of the event
+ * @returns 
+ * @since 1.0.0
+ */
+export const GhostEventManager = <T extends BaseEventsType>(event: GhostEvent<T>) => event
