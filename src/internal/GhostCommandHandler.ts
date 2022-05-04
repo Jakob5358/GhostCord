076a6c6119d690cc6a79b13a94e63a68b6443231
ThatGuyJamal <43446907+ThatGuyJamal@users.noreply.github.com>
2022-05-04 13:18:38 -0400

    commandEmitter

--- src/internal/GhostCommandHandler.ts
+++ src/internal/GhostCommandHandler.ts
@@ -1,29 +1,33 @@
 import { EventNames } from "../types/GhostEvent";
 import { GhostEvent } from "../structures/GhostEvent";
+import { GhostClient } from "../structures/GhostClient";
 
 /**
  * GhostCord Command Event Handler
  * @since 1.0.0
  */
 export default GhostEvent({
-  name: "interactionCreate",
-  async run(client, interaction) {
-    if (!interaction.isCommand()) return;
-    if (!interaction.inCachedGuild()) return client.emit("uncachedGuild", interaction);
+	name: "interactionCreate",
+  async run(client: GhostClient, interaction) {
+		if (!interaction.isCommand()) return;
+		if (!interaction.inCachedGuild()) return client.emit("uncachedGuild", interaction);
 
-    const command = client.commands.get(interaction.commandName);
-    if (!command) return client.emit(EventNames.COMMAND_NOT_FOUND, interaction);
+		const command = client.commands.get(interaction.commandName);
+		if (!command) {
+			client.commandEmitter(interaction)
+			return;
+		}
 
-    try {
-      await command.run(interaction, client);
-    } catch (err: any) {
-      client.emit(EventNames.COMMAND_EXCEPTION, interaction, err);
+		try {
+			await command.run(interaction, client);
+		} catch (err: any) {
+			client.emit(EventNames.COMMAND_EXCEPTION, interaction, err);
 
-      if (client.listeners(EventNames.COMMAND_EXCEPTION).length) return;
+			if (client.listeners(EventNames.COMMAND_EXCEPTION).length) return;
 
-      await interaction[interaction.deferred ? "editReply" : interaction.replied ? "followUp" : "reply"]({
-        content: `❌ ${err}`,
-      });
-    }
-  },
+			await interaction[interaction.deferred ? "editReply" : interaction.replied ? "followUp" : "reply"]({
+				content: `❌ ${err}`,
+			});
+		}
+	},
 });
--- src/structures/GhostClient.ts
+++ src/structures/GhostClient.ts
@@ -1,4 +1,4 @@
-import { Client, Collection } from "discord.js";
+import { Client, Collection, CommandInteraction } from "discord.js";
 import { GhostOptions } from "../types/GhostOptions";
 import { GhostCommand } from "./GhostCommand";
 import { GhostEventType } from "../types/GhostEvent";
@@ -10,57 +10,76 @@ import { container } from "./GhostContainer";
  * @since 1.0.0
  */
 export class GhostClient extends Client {
-  public commands = new Collection<string, GhostCommand>();
-  public constructor(public options: GhostOptions) {
-    super(options);
-    if (options.plugins) {
-      for (const plugin of options.plugins) {
-        container.PluginManager.pluginStore.set(plugin.name, plugin);
-      }
-      container.logger.debug("Loaded plugins into the plugin manager");
-    }
-  }
+	public commands = new Collection<string, GhostCommand>();
+	public constructor(public options: GhostOptions) {
+		super(options);
+		if (options.plugins) {
+			for (const plugin of options.plugins) {
+				container.PluginManager.pluginStore.set(plugin.name, plugin);
+			}
+			container.logger.debug("Loaded plugins into the plugin manager");
+		}
+	}
+
+	/**
+	 * Starts all base instances and utilities.
+	 * @since 1.0.0
+	 */
+	public async start() {
+		await this.registerCommands();
+		await this.registerEvents();
+		await this.login(this.options.token);
+	}
+
+	/**
+	 * Loads all commands into the client.
+	 * @since 1.0.0
+	 */
+	private async registerCommands() {
+		for await (const path of this.options.commands) {
+			const file = await import(path);
+			const command: GhostCommand = file.default;
+
+			let name: string = command.name.toLocaleLowerCase();
+
+			// check if name is over 25 characters
+			if (name.length > 25) {
+				name = name.substring(0, 25);
+				container.logger.warn(`Command ${command.name} is over 25 characters, truncated to ${name}`);
+			}
+
+			this.commands.set(name, command);
+			this.emit(EventNames.COMMAND_LOADED, command);
+		}
+	}
+
+	/**
+	 * Registers all events into the client.
+	 * @since 1.0.0
+	 */
+	public async registerEvents() {
+		for await (const path of this.options.events) {
+			const file = await import(path);
+			const event: GhostEventType<any> = file.default;
+
+			if (!event || !event.name || !event.run) {
+				container.logger.warn(`Event at ${path} doesn\'t seem to be correctly exporting an event`);
+				continue;
+			}
+
+			this[event.once ? "once" : "on"](event.name, (...args: any[]) => void event.run(this, ...args));
+			this.emit(EventNames.EVENT_LOADED, event);
+		}
+	}
 
   /**
-   * Starts all base instances and utilities.
-   * @since 1.0.0
+   * Emits an event based on the interaction.
+   * @param interaction The command interaction to emit.
    */
-  public async start() {
-    await this.registerCommands();
-    await this.registerEvents();
-    await this.login(this.options.token);
-  }
-
-  /**
-   * Loads all commands into the client.
-   * @since 1.0.0
-   */
-  private async registerCommands() {
-    for await (const path of this.options.commands) {
-      const file = await import(path);
-      const command: GhostCommand = file.default;
-
-      this.commands.set(command.name, command);
-      this.emit(EventNames.COMMAND_LOADED, command);
-    }
-  }
-
-  /**
-   * Registers all events into the client.
-   * @since 1.0.0
-   */
-  private async registerEvents() {
-    for await (const path of this.options.events) {
-      const file = await import(path);
-      const event: GhostEventType<any> = file.default;
-
-      if (!event || !event.name || !event.run) {
-        container.logger.warn(`Event at ${path} doesn\'t seem to be correctly exporting an event`)
-        continue;
-      }
-
-      this[event.once ? "once" : "on"](event.name, (...args: any[]) => void event.run(this, ...args));
-      this.emit(EventNames.EVENT_LOADED, event);
-    }
-  }
+	public commandEmitter(interaction: CommandInteraction<"cached">) {
+		this.on(EventNames.COMMAND_NOT_FOUND, () => {
+			container.logger.debug(`Command ${interaction.commandName} (${interaction.commandId}) not found`);
+		});
+		this.emit(EventNames.COMMAND_NOT_FOUND, interaction);
+	}
 }
